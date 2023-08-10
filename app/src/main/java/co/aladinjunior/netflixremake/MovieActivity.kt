21dc61602a5e2ec37e79e5a318d07d517be884a6
Aladin Júnior <aladinjr013@icloud.com>
2023-08-10 18:29:55 -0300

    change layerlist image programmatically added

--- app/src/main/java/co/aladinjunior/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/aladinjunior/netflixremake/MovieActivity.kt
@@ -1,8 +1,11 @@
 package co.aladinjunior.netflixremake
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,12 @@ class MovieActivity : AppCompatActivity() {
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.title = null
+
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.movie_background_shadows) as LayerDrawable
+        val bgDrawable = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        layerDrawable.setDrawableByLayerId(R.id.covered_movie_background, bgDrawable)
+        val movieBackground: ImageView = findViewById(R.id.movie_background)
+        movieBackground.setImageDrawable(layerDrawable)
 
     }
 }
